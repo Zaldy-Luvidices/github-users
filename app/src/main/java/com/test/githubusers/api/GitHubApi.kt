97c555c427fa997e5a9baa99f1987e4b1ca8b2b8
Zaldy Luvidices <luvidices.zaldy@gmail.com>
2026-01-21 20:21:46 +0900

    Added interface function for user details api.

--- app/src/main/java/com/test/githubusers/api/GitHubApi.kt
+++ app/src/main/java/com/test/githubusers/api/GitHubApi.kt
@@ -1,9 +1,13 @@
 package com.test.githubusers.api
 
+import com.test.githubusers.model.UserDetailModel
 import com.test.githubusers.model.UserModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GitHubApi {
     @GET("users")
     suspend fun getUsers(): List<UserModel>
+    @GET("users/{login}")
+    suspend fun getUserDetails(@Path("login") login: String): UserDetailModel
 }
--- app/src/main/java/com/test/githubusers/repository/UsersRepository.kt
+++ app/src/main/java/com/test/githubusers/repository/UsersRepository.kt
@@ -1,10 +1,14 @@
 package com.test.githubusers.repository
 
 import com.test.githubusers.api.ApiClient
+import com.test.githubusers.model.UserDetailModel
 import com.test.githubusers.model.UserModel
 
 class UsersRepository {
     suspend fun getUsers(): List<UserModel> {
         return ApiClient.api.getUsers()
+    }
+    suspend fun getUserDetails(login: String): UserDetailModel {
+        return ApiClient.api.getUserDetails(login)
     }
 }
