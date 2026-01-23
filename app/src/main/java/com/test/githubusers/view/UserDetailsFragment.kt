d006bed82a42d9dce001eac5c850f7c4461871c1
Zaldy Luvidices <luvidices.zaldy@gmail.com>
2026-01-23 14:47:04 +0900

    Added more information to user details fragment.

--- app/src/main/java/com/test/githubusers/view/UserDetailsFragment.kt
+++ app/src/main/java/com/test/githubusers/view/UserDetailsFragment.kt
@@ -1,9 +1,12 @@
 package com.test.githubusers.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -15,16 +18,36 @@ import com.test.githubusers.viewmodel.UsersViewModel
 
 class UserDetailsFragment : Fragment(R.layout.fragment_user_details) {
 
-    private var imgAvatar: ImageView? = null
-    private var txtName: TextView? = null
+    private lateinit var imgAvatar: ImageView
+    private lateinit var txtName: TextView
+    private lateinit var txtUsername: TextView
+    private lateinit var txtCompany: TextView
+    private lateinit var txtLocation: TextView
+    private lateinit var txtBio: TextView
+    private lateinit var txtRepos: TextView
+    private lateinit var txtFollowers: TextView
+    private lateinit var txtFollowing: TextView
+    private lateinit var txtGithubLink: TextView
+    private lateinit var txtBlog: TextView
 
     private val usersViewModel: UsersViewModel by viewModels()
     private val sharedViewModel: MainSharedViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         imgAvatar = view.findViewById(R.id.imgAvatar)
         txtName = view.findViewById(R.id.txtName)
+        txtUsername = view.findViewById(R.id.txtUsername)
+        txtCompany = view.findViewById(R.id.txtCompany)
+        txtLocation = view.findViewById(R.id.txtLocation)
+        txtBio = view.findViewById(R.id.txtBio)
+        txtRepos = view.findViewById(R.id.txtRepos)
+        txtFollowers = view.findViewById(R.id.txtFollowers)
+        txtFollowing = view.findViewById(R.id.txtFollowing)
+        txtGithubLink = view.findViewById(R.id.txtGithubLink)
+        txtBlog = view.findViewById(R.id.txtBlog)
+
         usersViewModel.userDetails.observe(viewLifecycleOwner) {
             this.loadDetails(it)
         }
@@ -33,8 +56,32 @@ class UserDetailsFragment : Fragment(R.layout.fragment_user_details) {
         }
     }
 
-    private fun loadDetails(userDetails: UserDetailModel) {
-        imgAvatar?.load(userDetails.avatarUrl)
-        txtName?.text = userDetails.name
+    private fun loadDetails(user: UserDetailModel) {
+        imgAvatar.load(user.avatarUrl)
+        txtName.text = user.name ?: user.login
+        txtUsername.text = user.login
+        txtCompany.isVisible = !user.company.isNullOrBlank()
+        txtCompany.text = user.company
+        txtLocation.isVisible = !user.location.isNullOrBlank()
+        txtLocation.text = user.location
+        txtBio.isVisible = !user.bio.isNullOrBlank()
+        txtBio.text = user.bio
+        txtRepos.text = getString(R.string.repos, user.publicRepos)
+        txtFollowers.text = getString(R.string.followers, user.followers)
+        txtFollowing.text = getString(R.string.following, user.following)
+        txtGithubLink.setOnClickListener {
+            openUrl(user.htmlUrl)
+        }
+        if (!user.blog.isNullOrBlank()) {
+            txtBlog.isVisible = true
+            txtBlog.text = user.blog
+            txtBlog.setOnClickListener { openUrl(user.blog) }
+        } else {
+            txtBlog.isVisible = false
+        }
+    }
+
+    private fun openUrl(url: String) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }
